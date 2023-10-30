c80014d3e955b591ec6e87277570d87e28a79f12
LucasRocha <lucassrocha096@gmail.com>
2023-10-30 19:17:22 +0000

    Added error handling classes, Edit and Delete to backend

--- src/main/java/com/LucasRocha/fullstackbackend/controller/UserController.java
+++ src/main/java/com/LucasRocha/fullstackbackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.LucasRocha.fullstackbackend.controller;
 
+import com.LucasRocha.fullstackbackend.exception.UserNotFoundException;
 import com.LucasRocha.fullstackbackend.model.User;
 import com.LucasRocha.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +23,42 @@ public class UserController {
     List<User> getAllUsers(){
         return userRepository.findAll();
     }
+
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable long id){
+
+        return userRepository.findById(id).orElseThrow(()-> new UserNotFoundException(id));
+    };
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id).map(user ->{
+            user.setUsername(newUser.getUsername());
+            user.setName(newUser.getName());
+            user.setEmail(newUser.getEmail());
+            return userRepository.save(user);
+
+        }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User whit "+id+" has been deleted successly.";
+    }
+
+
+
+
+
+
+
+
+
 }
---
+++ src/main/java/com/LucasRocha/fullstackbackend/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.LucasRocha.fullstackbackend.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(long id){
+        super("Could not found the user id "+ id);
+    }
+}
